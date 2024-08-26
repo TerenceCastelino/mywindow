47bc02fef4e50f28e0431568bc0230096cfa0fb6
terence planchestainer <t.planchestainer@stag.technofuturtic.education>
2024-08-26 22:30:54 +0200

    init nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './_Pages/_Layout/_Navbar/Navbar';
+import Home from './_Pages/_Home/Home';
 import './App.css';
-
 
 function App() {
   return (
     <div>
+      <Navbar />
+      <Routes>
+        <Route path="/ff" element={<Home />} />
+
+      </Routes>
+
 
     </div>
   );
---
+++ src/_Pages/_Layout/_Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import "./Navbar.css";
+
+function Navbar(props) {
+    return (
+        <div >
+
+            <h1>naaaaaaaaaaaavvvvvvvvvvvvvv</h1>
+            <Link to='/ff'>Acceuil</Link>
+
+        </div>
+    );
+}
+
+export default Navbar;
